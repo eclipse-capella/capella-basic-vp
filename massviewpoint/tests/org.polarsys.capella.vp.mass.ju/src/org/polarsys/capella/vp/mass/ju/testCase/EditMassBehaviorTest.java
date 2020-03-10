100fbbb7e2aeade3ff0ce6fe23a57cdccbe72c34
Raphaël Pagé <raphael.page@obeo.fr>
2020-03-10 17:22:31 +0100

    Add JUnit test cases for mass computation
    The test suite checks if the mass computation is correctly computed when
    opening a new session, and if the computation is triggered after
    specific events :
    -adding a new mass to the model
    -removing an element or its mass
    -editing the mass of an element
    
    Change-Id: Ic1da8726cf606de7dc9f8c5ea35adec5017bc12a
    Signed-off-by: Raphaël Pagé <raphael.page@obeo.fr>

---
+++ massviewpoint/tests/org.polarsys.capella.vp.mass.ju/src/org/polarsys/capella/vp/mass/ju/testCase/EditMassBehaviorTest.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Obeo
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *   Contributors:
+ *      OBEO - initial API and implementation
+ ******************************************************************************/
+package org.polarsys.capella.vp.mass.ju.testCase;
+
+import org.polarsys.capella.core.data.cs.impl.PartImpl;
+import org.polarsys.capella.core.data.pa.PhysicalComponent;
+import org.polarsys.capella.vp.mass.mass.impl.PartMassImpl;
+
+/**
+ * This test case checks if when editing the mass of a behavior node the mass of
+ * its parents is re-calculated
+ */
+public class EditMassBehaviorTest extends MassTest {
+	PartMassImpl pc1PartMass;
+	PartMassImpl pc121PartMass;
+
+	@Override
+	public void test() throws Exception {
+		PhysicalComponent pc1 = physicalSystem.getOwnedPhysicalComponents().get(0);
+		pc1PartMass = (PartMassImpl) ((PartImpl) pc1.getAbstractTypedElements().get(0)).getOwnedExtensions().get(0);
+		PhysicalComponent pc12 = pc1.getDeployedPhysicalComponents().get(0);
+		PhysicalComponent pc121 = pc12.getDeployedPhysicalComponents().get(0);
+		pc121PartMass = (PartMassImpl) ((PartImpl) pc121.getAbstractTypedElements().get(0)).getOwnedExtensions().get(0);
+
+		// change the mass of pc1.2.1 (a behavior) and check if the mass of pc1 is
+		// re-calculated
+		editMassPhysicalComponent(pc121, 10);
+		assertEquals("The mass of PC1.2.1 was not changed after its modification", pc121PartMass.getCurrentMass(), 10);
+		assertEquals("The mass of PC1 was not changed after the modification of pc1.2.1", pc1PartMass.getCurrentMass(),
+				90);
+
+	}
+
+}
