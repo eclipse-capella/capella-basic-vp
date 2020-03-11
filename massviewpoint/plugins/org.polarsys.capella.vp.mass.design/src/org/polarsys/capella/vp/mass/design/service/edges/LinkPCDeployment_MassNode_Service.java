3eaac9cfb3d3dee651f7993f8f6a79aad458fc2b
Raphaël Pagé <raphael.page@obeo.fr>
2020-03-11 17:17:47 +0100

    Correct typo in copyright comments
    "Obeo" instead of "OBEO"
    
    Change-Id: Ief0aa8b8ea0f936739b5e5d5d4eb5c9ef4743056
    Signed-off-by: Raphaël Pagé <raphael.page@obeo.fr>

--- massviewpoint/plugins/org.polarsys.capella.vp.mass.design/src/org/polarsys/capella/vp/mass/design/service/edges/LinkPCDeployment_MassNode_Service.java
+++ massviewpoint/plugins/org.polarsys.capella.vp.mass.design/src/org/polarsys/capella/vp/mass/design/service/edges/LinkPCDeployment_MassNode_Service.java
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (c) 2006, 2016 Thales Global Services 
+* Copyright (c) 2006, 2020 Thales Global Services 
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0 
  * which accompanies this distribution, and is available at 
@@ -7,6 +7,7 @@
  * 
  * Contributors: 
  *    Thales - initial API and implementation
+ *    Obeo - code improvement
 *****************************************************************************/
 package org.polarsys.capella.vp.mass.design.service.edges;
 
