bddea8f6b2f661c2c1b262017173c3d21615196b
Raphaël Pagé <raphael.page@obeo.fr>
2020-03-11 17:17:47 +0100

    Correct typo in copyright comments
    "Obeo" instead of "OBEO"
    
    Change-Id: Ief0aa8b8ea0f936739b5e5d5d4eb5c9ef4743056
    Signed-off-by: Raphaël Pagé <raphael.page@obeo.fr>

--- massviewpoint/plugins/org.polarsys.capella.vp.mass.design/src/org/polarsys/capella/vp/mass/design/service/massListener/MassChangePreCommitListener.java
+++ massviewpoint/plugins/org.polarsys.capella.vp.mass.design/src/org/polarsys/capella/vp/mass/design/service/massListener/MassChangePreCommitListener.java
@@ -6,7 +6,7 @@
  *   http://www.eclipse.org/legal/epl-v10.html
  * 
  *   Contributors:
- *      OBEO - initial API and implementation
+ *      Obeo - initial API and implementation
  ******************************************************************************/
 package org.polarsys.capella.vp.mass.design.service.massListener;
 
