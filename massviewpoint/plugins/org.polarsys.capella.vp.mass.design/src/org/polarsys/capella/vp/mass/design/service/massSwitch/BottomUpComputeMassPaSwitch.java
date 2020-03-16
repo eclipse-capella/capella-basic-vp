93cee8632e4ab961075545978eed3dcc0293ae55
Raphaël Pagé <raphael.page@obeo.fr>
2020-03-16 14:08:32 +0100

    Only compute mass of parents if mass changed
    It is unnecessary to call the mass computation of the parents of a
    component if the mass of the component didn't change after being
    computed.
    
    Change-Id: Id4b77b445056d72eb0ce8002983b840e982f0c72
    Signed-off-by: Raphaël Pagé <raphael.page@obeo.fr>

--- massviewpoint/plugins/org.polarsys.capella.vp.mass.design/src/org/polarsys/capella/vp/mass/design/service/massSwitch/BottomUpComputeMassPaSwitch.java
+++ massviewpoint/plugins/org.polarsys.capella.vp.mass.design/src/org/polarsys/capella/vp/mass/design/service/massSwitch/BottomUpComputeMassPaSwitch.java
@@ -11,11 +11,14 @@
 package org.polarsys.capella.vp.mass.design.service.massSwitch;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
 import org.polarsys.capella.common.data.modellingcore.AbstractTypedElement;
 import org.polarsys.capella.core.data.cs.Part;
 import org.polarsys.capella.core.data.pa.PhysicalComponent;
 import org.polarsys.capella.core.data.pa.PhysicalComponentNature;
 import org.polarsys.capella.core.data.pa.PhysicalComponentPkg;
+import org.polarsys.capella.vp.mass.helpers.MassHelper;
+import org.polarsys.capella.vp.mass.mass.PartMass;
 
 /**
  * This switch is used to trigger the mass computation of a parent after one of
@@ -25,27 +28,44 @@ public class BottomUpComputeMassPaSwitch extends ComputeMassSwitch {
 
 	@Override
 	public Void casePhysicalComponent(PhysicalComponent physicalComponent) {
-		computeMass(physicalComponent);
 		
-		// The parent of a behavior component is not his container. The link is made
-		// through its deployingParts
-		if (physicalComponent.getNature() == PhysicalComponentNature.BEHAVIOR) {
-			EList<AbstractTypedElement> abstractTypedElements = physicalComponent.getAbstractTypedElements();
-
-			for (AbstractTypedElement part : abstractTypedElements) {
-				EList<Part> deployingParts = ((Part) part).getDeployingParts();
-				for (Part deployingPart : deployingParts) {
-					this.doSwitch(deployingPart.getAbstractType());
-				}
+		MassHelper massHelper = new MassHelper();
+		EList<EObject> massObjects = massHelper.getMassObjects(physicalComponent);
+		boolean computeParents = true;
+		
+		//if the component has a mass, we have to compute it
+		if(massObjects.size() != 0) {
+			int oldMass = ((PartMass)(massObjects.get(0))).getCurrentMass();
+			computeMass(physicalComponent);
+			int currentMass = ((PartMass)(massObjects.get(0))).getCurrentMass();
+			
+			//if the mass didn't changed, we do not need to go further up the tree
+			if(oldMass == currentMass) {
+				computeParents=false;
 			}
-		} else {
-			this.doSwitch(physicalComponent.eContainer());
+		}
+		
+		if(computeParents) {
+			if (physicalComponent.getNature() == PhysicalComponentNature.BEHAVIOR) {
+				//the parents of a behavior node are accessible through its abstract elements
+				EList<AbstractTypedElement> abstractTypedElements = physicalComponent.getAbstractTypedElements();
+
+				for (AbstractTypedElement part : abstractTypedElements) {
+					EList<Part> deployingParts = ((Part) part).getDeployingParts();
+					for (Part deployingPart : deployingParts) {
+						this.doSwitch(deployingPart.getAbstractType());
+					}
+				}
+			} else {
+				this.doSwitch(physicalComponent.eContainer());
+			}
 		}
 		return null;
 	}
 
 	@Override
 	public Void casePhysicalComponentPkg(PhysicalComponentPkg physicalComponentPkg) {
+		//a pkg can't have a mass
 		this.doSwitch(physicalComponentPkg.eContainer());
 		return null;
 	}
