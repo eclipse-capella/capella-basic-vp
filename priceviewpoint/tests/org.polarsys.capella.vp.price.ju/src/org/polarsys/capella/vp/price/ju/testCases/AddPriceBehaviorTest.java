e4e4642c4bbe0336ec441b02541bd90c00788dd1
Josselin Enet <josselin.enet@obeo.fr>
2021-02-18 11:58:37 +0100

    Create tests for Price Viewpoint
    Adapt the Mass Viewpoint tests to Price Viewpoint.
    
    Signed-off-by: Josselin Enet <josselin.enet@obeo.fr>

---
+++ priceviewpoint/tests/org.polarsys.capella.vp.price.ju/src/org/polarsys/capella/vp/price/ju/testCases/AddPriceBehaviorTest.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Obeo
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *   Contributors:
+ *      Obeo - initial API and implementation
+ ******************************************************************************/
+package org.polarsys.capella.vp.price.ju.testCases;
+
+import org.polarsys.capella.core.data.cs.impl.PartImpl;
+import org.polarsys.capella.core.data.pa.PhysicalComponent;
+import org.polarsys.capella.core.data.pa.impl.PhysicalComponentImpl;
+import org.polarsys.capella.vp.price.price.impl.PartPriceImpl;
+
+/**
+ * This test case checks if when adding a price to a Behavior PC the price of its
+ * parents are re-calculated.
+ * Used to check if the listener responds to the notification ADD
+ */
+public class AddPriceBehaviorTest extends PriceTest {
+	PartPriceImpl pc1PartPrice;
+
+	@Override
+	public void test() throws Exception {
+
+		PhysicalComponent pc1 = physicalSystem.getOwnedPhysicalComponents().get(0);
+		pc1PartPrice = (PartPriceImpl) ((PartImpl) pc1.getAbstractTypedElements().get(0)).getOwnedExtensions().get(0);
+		PhysicalComponentImpl pc1_4 = (PhysicalComponentImpl) pc1.getDeployedPhysicalComponents().get(1);
+
+		// add a price to pc1.4 and check if the price of pc1 is re-calculated
+		addPriceToPhysicalComponent(pc1_4, 10);
+		assertEquals("The price of PC1 was not changed after the addition of a mass to pc1.4", pc1PartPrice.getCurrentPrice(), 95);
+
+	}
+}
---
+++ priceviewpoint/tests/org.polarsys.capella.vp.price.ju/src/org/polarsys/capella/vp/price/ju/testSuites/PriceSessionListenerTestSuite.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Obeo
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *   Contributors:
+ *      Obeo - initial API and implementation
+ ******************************************************************************/
+package org.polarsys.capella.vp.price.ju.testSuites;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.polarsys.capella.test.framework.api.BasicTestArtefact;
+import org.polarsys.capella.test.framework.api.BasicTestSuite;
+import org.polarsys.capella.vp.price.ju.testCases.AddOneBehaviorOneNodeWithPriceTest;
+import org.polarsys.capella.vp.price.ju.testCases.AddPriceBehaviorTest;
+import org.polarsys.capella.vp.price.ju.testCases.AddPriceNodeTest;
+import org.polarsys.capella.vp.price.ju.testCases.DeleteBehaviorWithPriceTest;
+import org.polarsys.capella.vp.price.ju.testCases.DeleteManyNodesWithPriceTest;
+import org.polarsys.capella.vp.price.ju.testCases.DeleteNodeWithPriceTest;
+import org.polarsys.capella.vp.price.ju.testCases.DeletePartPriceBehaviorTest;
+import org.polarsys.capella.vp.price.ju.testCases.DeletePartPriceNodeTest;
+import org.polarsys.capella.vp.price.ju.testCases.EditPriceBehaviorTest;
+import org.polarsys.capella.vp.price.ju.testCases.EditPriceNodeTest;
+import org.polarsys.capella.vp.price.ju.testCases.PriceCalculatedOpeningSessionTest;
+
+import junit.framework.Test;
+
+/**
+ * This test suite starts tests that check whether the price listeners are
+ * correctly installed, and if the price of the elements is correctly calculated
+ * after certain events
+ */
+public class PriceSessionListenerTestSuite extends BasicTestSuite {
+
+	/**
+	 * Returns the suite. This is required to unary launch this test.
+	 */
+	public static Test suite() {
+		return new PriceSessionListenerTestSuite();
+	}
+
+	@Override
+	protected List<BasicTestArtefact> getTests() {
+		List<BasicTestArtefact> tests = new ArrayList<BasicTestArtefact>();
+		tests.add(new PriceCalculatedOpeningSessionTest());
+		tests.add(new EditPriceNodeTest());
+		tests.add(new EditPriceBehaviorTest());
+		tests.add(new AddPriceNodeTest());
+		tests.add(new AddPriceBehaviorTest());
+		tests.add(new DeleteNodeWithPriceTest());
+		tests.add(new DeleteBehaviorWithPriceTest());
+		tests.add(new DeletePartPriceBehaviorTest());
+		tests.add(new DeletePartPriceNodeTest());
+		tests.add(new AddOneBehaviorOneNodeWithPriceTest());
+		tests.add(new DeleteManyNodesWithPriceTest());
+		return tests;
+	}
+
+}
