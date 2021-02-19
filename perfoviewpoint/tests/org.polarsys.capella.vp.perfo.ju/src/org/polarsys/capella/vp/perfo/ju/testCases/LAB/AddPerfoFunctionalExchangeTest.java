7580363c5e70b96ea7234dcc5ea65ed2ebb4a910
Josselin Enet <josselin.enet@obeo.fr>
2021-02-19 17:25:22 +0100

    Add tests cases for LAB diagram from Perfo Viewpoint
    Signed-off-by: Josselin Enet <josselin.enet@obeo.fr>

---
+++ perfoviewpoint/tests/org.polarsys.capella.vp.perfo.ju/src/org/polarsys/capella/vp/perfo/ju/testCases/LAB/AddPerfoFunctionalExchangeTest.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Obeo
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *   Contributors:
+ *      Obeo - initial API and implementation
+ ******************************************************************************/
+package org.polarsys.capella.vp.perfo.ju.testCases.LAB;
+
+import org.polarsys.capella.core.data.fa.FunctionalChain;
+import org.polarsys.capella.core.data.fa.FunctionalExchange;
+import org.polarsys.capella.vp.perfo.perfo.impl.TimeCapacityImpl;
+
+/**
+ * This test case checks if when adding a perfo to a FunctionalExchange the perfo of the
+ * FunctionalChains it is a part of are re-calculated.
+ * Used to check if the listener responds to the notification ADD
+ */
+public class AddPerfoFunctionalExchangeTest extends LABPerfoTest {
+
+	@Override
+	public void test() throws Exception {
+		FunctionalExchange functionalExchange1 = rootAbstractFunction.getOwnedFunctionalExchanges().get(0);
+		FunctionalChain functionalChain = functionalExchange1.getInvolvingFunctionalChains().get(0);
+		
+		addTimeConsumption(functionalExchange1, 40);
+		assertEquals("The perfo of FC1 was not changed after the addition of a perfo to FE1",
+				((TimeCapacityImpl) functionalChain.getOwnedExtensions().get(0)).getCurrentExecutionTime(), 150);
+
+	}
+
+}
---
+++ perfoviewpoint/tests/org.polarsys.capella.vp.perfo.ju/src/org/polarsys/capella/vp/perfo/ju/testCases/LAB/DeleteFunctionalExchangeFromFunctionalChainTest.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Obeo
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *   Contributors:
+ *      Obeo - initial API and implementation
+ ******************************************************************************/
+package org.polarsys.capella.vp.perfo.ju.testCases.LAB;
+
+import org.polarsys.capella.common.ef.ExecutionManager;
+import org.polarsys.capella.common.ef.command.AbstractReadWriteCommand;
+import org.polarsys.capella.common.helpers.TransactionHelper;
+import org.polarsys.capella.core.data.fa.FunctionalChain;
+import org.polarsys.capella.core.data.fa.FunctionalChainInvolvement;
+import org.polarsys.capella.test.framework.helpers.EObjectHelper;
+import org.polarsys.capella.vp.perfo.perfo.impl.TimeCapacityImpl;
+
+/**
+ * This test case checks if when deleting a FunctionalExchange from a
+ * FunctionalChain, the perfo of the FunctionalChain is re-calculated.
+ * Used to check if the listener responds to the notification REMOVE
+ */
+public class DeleteFunctionalExchangeFromFunctionalChainTest extends LABPerfoTest {
+
+	@Override
+	public void test() throws Exception {
+		FunctionalChain functionalChain1 = rootAbstractFunction.getOwnedFunctionalChains().get(0);
+		FunctionalChainInvolvement functionalChainInvolvement = functionalChain1.getInvolvedFunctionalChainInvolvements().get(1);
+		
+		ExecutionManager manager = TransactionHelper.getExecutionManager(functionalChainInvolvement);
+		manager.execute(new AbstractReadWriteCommand() {
+
+			@Override
+			public void run() {
+				EObjectHelper.removeElement(functionalChainInvolvement);
+			}
+		});
+		
+		assertEquals("The performance of FC1 was not changed after the deletion of FE2 from the functional chain",
+				((TimeCapacityImpl) functionalChain1.getOwnedExtensions().get(0)).getCurrentExecutionTime(), 80);
+
+	}
+
+}
