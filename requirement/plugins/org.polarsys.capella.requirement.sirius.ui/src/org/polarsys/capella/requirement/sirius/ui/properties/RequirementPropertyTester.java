68066cdd3fc7b4cde5047a634c7f8b0efdf43755
Erwann Traisnel <erwann.traisnel@obeo.fr>
2023-08-03 17:34:18 +0200

    #71 Requirement Manager Wizard
    Change-Id: I346d79aa535f1ac341a54702fdb252ddc1d95c73
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

---
+++ requirement/plugins/org.polarsys.capella.requirement.sirius.ui/src/org/polarsys/capella/requirement/sirius/ui/properties/RequirementPropertyTester.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2020 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.requirement.sirius.ui.properties;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.polarsys.capella.common.data.modellingcore.ModelElement;
+import org.polarsys.capella.common.ui.actions.ModelAdaptation;
+import org.polarsys.capella.core.data.capellacore.CapellaElement;
+import org.polarsys.capella.core.data.cs.Component;
+import org.polarsys.capella.core.data.cs.Part;
+import org.polarsys.capella.core.data.epbs.ConfigurationItem;
+import org.polarsys.capella.core.data.fa.AbstractFunction;
+import org.polarsys.capella.core.data.fa.ComponentExchange;
+import org.polarsys.capella.core.data.fa.FunctionalExchange;
+import org.polarsys.capella.core.data.information.datavalue.LiteralNumericValue;
+import org.polarsys.capella.core.model.utils.CapellaLayerCheckingExt;
+
+/**
+ *
+ */
+public class RequirementPropertyTester extends PropertyTester {
+
+  /**
+   * @see org.eclipse.core.expressions.IPropertyTester#test(java.lang.Object, java.lang.String, java.lang.Object[],
+   *      java.lang.Object)
+   */
+  public boolean test(Object object_p, String propertyName_p, Object[] params_p, Object testedValue_p) {
+    boolean result = false;
+    if (propertyName_p.equals("actionMode") || propertyName_p.equals("graphicalActionMode")) { //$NON-NLS-1$ //$NON-NLS-2$
+      // getting the capella element
+      ModelElement element = ModelAdaptation.adaptToCapella(object_p);
+      if ((element != null) && (element instanceof CapellaElement)) {
+        String actionName = (String) testedValue_p;
+        
+        if ("requirementManagerWizard".equals(actionName)) { //$NON-NLS-1$
+          return isRequirementManagerWizard(element);
+        }
+      }
+    }
+    return result;
+  }
+  private boolean isRequirementManagerWizard(ModelElement element) {
+    return true;
+  }
+}
