76aee2d833e84d5d04e359e581c83cc48cc9de74
Erwann Traisnel <erwann.traisnel@obeo.fr>
2023-09-01 12:16:06 +0200

    #74 Contribute RequirementsTrace to Capella Trace Manager
    Change-Id: I914dff20f334a1dcfdfe9ce4446a960e858345d4
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

---
+++ requirement/plugins/org.polarsys.capella.basic.requirement.trace/src/org/polarsys/capella/basic/requirement/trace/extension/RequirementTraceExtension.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2023 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.basic.requirement.trace.extension;
+
+import org.polarsys.capella.basic.requirement.RequirementFactory;
+import org.polarsys.capella.basic.requirement.RequirementsTrace;
+import org.polarsys.capella.core.data.capellacore.Trace;
+import org.polarsys.capella.core.platform.eclipse.capella.ui.trace.extension.ITraceExtension;
+
+public class RequirementTraceExtension implements ITraceExtension {
+
+  @Override
+  public boolean canDelete(Trace element) {
+    return isRequirementsTrace(element);
+  }
+
+  @Override
+  public boolean canRemoveSource(Trace element) {
+    return isRequirementsTrace(element);
+  }
+
+  @Override
+  public boolean canRemoveTarget(Trace element) {
+    return isRequirementsTrace(element);
+  }
+
+  public boolean isRequirementsTrace(Object element) {
+    return element instanceof RequirementsTrace;
+  }
+
+  @Override
+  public boolean isAssignableFrom(Class<?> clazz) {
+    return RequirementsTrace.class.isAssignableFrom(clazz);
+  }
+
+  @Override
+  public String getTraceName() {
+    return Messages.requirementTraceType;
+  }
+
+  @Override
+  public Trace getNewTraceInstance() {
+    return RequirementFactory.eINSTANCE.createRequirementsTrace();
+  }
+
+  @Override
+  public boolean isManualTrace() {
+    return true;
+  }
+
+  @Override
+  public boolean canAddRemoveItemsToTrace(Object element) {
+    return isRequirementsTrace(element);
+  }
+
+  @Override
+  public boolean canEdit(Object element) {
+    return isRequirementsTrace(element);
+  }
+
+}
